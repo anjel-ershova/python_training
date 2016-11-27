ef7fb20aae889e5f5d78b75b9d1d757e3dd60fad
Anjel Ershova <a.ershova@abbyy-ls.com>
2016-11-27 21:06:30 +0300

    Задание 11 - сортировка списков перед сравнением (4_1, 4_2, 4_3)
    правка по test_delete_first_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,10 +1,10 @@
 from model.model_contact import *
 
-def test_add_contact(app2):
+def test_delete_first_contact(app2):
     if app2.contact.count() == 0:
-        app2.contact.create(General(firstname="firstname-created", middlename="middlename-created", lastname="lastname-created", nickname="nickname-created", title="title-created",
-                 company="company-created", address="address-created"), Telephone(home='home-created', mobile='mobile-created', work='work-created', fax='fax-created'),
-                 Email(email='email-created@mfsa.ru', email2='email2-created@mfsa.ru', email3='email3-created@mfsa.ru'), Secondary(address2='Another address-created', home='home_secondary-created', notes='Some text-created'))
+        app2.contact.create(Contact(firstname2="firstname-created", middlename="middlename-created", lastname="lastname-created", nickname="nickname-created", title="title-created",
+                company="company-created", address="address-created", home='home-created', mobile='mobile-created', work='work-created', fax='fax-created',
+                email='email-created@mfsa.ru', email2='email2-created@mfsa.ru', email3='email3-created@mfsa.ru', address2='Another address-created', home2='home_secondary-created', notes='Some text-created'))
     old_contacts = app2.contact.get_contact_list()
     app2.contact.delete_first_contact()
     new_contacts = app2.contact.get_contact_list()
