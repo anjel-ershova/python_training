3a1d548014e6a46443ac66c2888a3ffb5b58e0cc
Anjel Ershova <a.ershova@abbyy-ls.com>
2016-11-30 00:05:29 +0300

    5_5 ddt - adding groups adnd contacts with random parameters

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,41 +1,46 @@
 # -*- coding: utf-8 -*-
 from model.model_contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app2):
+def random_string(prefix, maxlen):
+    cont_symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+
+    return prefix + "".join([random.choice(cont_symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname2="", middlename="", lastname="", nickname="", title="",
+                           company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
+                           address2="", phone2="", notes="")] + [Contact
+                            (firstname2=random_string("firstname", 10),
+                             middlename=random_string("middlename", 20),
+                             lastname=random_string("lastname", 20),
+                             nickname=random_string("nickname", 20),
+                             title=random_string("title", 20),
+                             company=random_string("company", 20),
+                             address=random_string("address", 30),
+                             home=random_string("homephone", 20), mobile=random_string("mobilephone", 20), work=random_string("workphone", 20),
+                             fax=random_string("fax", 20),
+                             email=random_string("email1", 20), email2=random_string("email2", 20), email3=random_string("email3", 20),
+                             address2=random_string("Another_address", 20),
+                             phone2=random_string("home_secondary", 20),
+                             notes=random_string("notes", 50))
+for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app2, contact):
     # собираем старый список контактов
     old_contacts = app2.contact.get_contact_list()
-    # выносим данные контакта в локальную переменную
-    contact_data = Contact(firstname2="firstname2", middlename="middlename", lastname="lastname", nickname="nickname", title="title",
-                           company="company", address="address", home="555-5678", mobile="8-800-200-555-500", work="555-work", fax="812-123-23-34", email="email@mfsa.ru",
-                           email2="email2@mfsa.ru", email3="email3@mfsa.ru",
-                           address2="Another address", phone2="home_secondary", notes="Some text")
     # выполняем создание контакта в соответствии с локальными переменными
-    app2.contact.create(contact_data)
+    app2.contact.create(contact)
     # сравниваем старый и новый списки по длине, новый == старый +1, длину старого списка берем методом count
     assert len(old_contacts) + 1 == app2.contact.count()
     # собираем новый список контактов (после создания нового)
     new_contacts = app2.contact.get_contact_list()
     #добавляем в старый список данные от нового созданного контакта (из локальных переменных)
-    old_contacts.append(contact_data)
+    old_contacts.append(contact)
     # сравниваем 2 полученных списка: новый и старый с добавленным контактом
     # для того, чтобы их можно было сравнивать в model_contacts надо добавить 2 метода: __eq__ и id_or_max
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-def test_add_empty_contact(app2):
-    old_contacts = app2.contact.get_contact_list()
-    contact_data = Contact(firstname2="", middlename="", lastname="", nickname="", title="",
-                           company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
-                           address2="", phone2="", notes="")
-    # все же заполняется картинка, сайт и даты, т.к. для них не было вынесего отдельных методов в model,
-    # а сами изменения делаются напрямую в методе fill_contact_form
-    app2.contact.create(contact_data)
-    assert len(old_contacts) + 1 == app2.contact.count()
-    new_contacts = app2.contact.get_contact_list()
-    old_contacts.append(contact_data)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-
-
-
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,14 +6,12 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Group(name="", footer="", header="")] + [
-    Group(name=name, footer=footer, header=header)
-    for name in ["", random_string("name", 10)]
-    for footer in ["", random_string("footer", 20)]
-    for header in ["", random_string("header", 20)]
+    Group(name=random_string("name", 10), footer=random_string("footer", 20), header=random_string("header", 20))
+    for i in range(5)
 ]
 
 # 1 - название параметра, куда передаются тест. данные,
@@ -21,11 +19,10 @@ testdata = [Group(name="", footer="", header="")] + [
 # 3 - список с текстовым предлставлением тест. данных
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app2, group):
-    pass
-    #old_groups = app2.group.get_group_list()
-    #app2.group.create(group)
-    #assert len(old_groups) + 1 == app2.group.count()
-    #new_groups = app2.group.get_group_list()
-    #old_groups.append(group)
-    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups = app2.group.get_group_list()
+    app2.group.create(group)
+    assert len(old_groups) + 1 == app2.group.count()
+    new_groups = app2.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
