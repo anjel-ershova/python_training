466594377464b3d885bf66fa184ac1edcc046d2f
Anjel Ershova <a.ershova@abbyy-ls.com>
2016-11-29 22:18:16 +0300

    ddt v1 - цикл внутри тестовой функции

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,22 +2,14 @@
 from model.model_group import *
 
 def test_add_group(app2):
-    old_groups = app2.group.get_group_list()
-    group = Group(name="group_name1", footer="group_footer1", header="group_header1", id=None)
-    app2.group.create(group)
-    assert len(old_groups) + 1 == app2.group.count()
-    new_groups = app2.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    testdata = [Group(name="group_name1", footer="group_footer1", header="group_header1", id=None),
+            Group(name="", footer="", header="")
+]
+    for group in testdata:
+        old_groups = app2.group.get_group_list()
+        app2.group.create(group)
+        assert len(old_groups) + 1 == app2.group.count()
+        new_groups = app2.group.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-"""
-def test_add_empty_group(app2):
-    old_groups = app2.group.get_group_list()
-    group2 = Group(name="", footer="", header="")
-    app2.group.create(group2)
-    new_groups = app2.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group2)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-"""
