a719155de7fac3821d6f0389c9b3bfca8ad9b587
Anjel Ershova <a.ershova@abbyy-ls.com>
2016-11-29 22:52:49 +0300

    ddt v3.1 - 5 тестовых рандомных групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,9 +9,9 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
-    Group(name=random_string("name", 10), footer=random_string("footer", 20), header=random_string("header", 20)),
-    Group(name="", footer="", header="")
+testdata = [Group(name="", footer="", header="")] + [
+    Group(name=random_string("name", 10), footer=random_string("footer", 20), header=random_string("header", 20))
+    for i in range(5)
 ]
 
 # 1 - название параметра, куда передаются тест. данные,
