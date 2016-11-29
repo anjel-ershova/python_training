b9cc7d87f39dfd42e455dc313ad775040ccbb94a
Anjel Ershova <a.ershova@abbyy-ls.com>
2016-11-29 22:34:20 +0300

    ddt v2.1
    улучшен вывод использованных данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,11 @@ testdata = [Group(name="group_name1", footer="group_footer1", header="group_head
         Group(name="", footer="", header="")
 ]
 
-@pytest.mark.parametrize("group", testdata) # 1 - название параметра, куда передаются тест. данные, 2 - откуда тест. данные брать
+
+# 1 - название параметра, куда передаются тест. данные,
+# 2 - откуда тест. данные брать,
+# 3 - список с текстовым предлставлением тест. данных
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app2, group):
     pass
     #old_groups = app2.group.get_group_list()
